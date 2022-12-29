871aa8053420aefa21d984764cdae6754a0405ac
tayvol <115339691+tayvol@users.noreply.github.com>
2022-12-30 00:14:21 +0530

    Add files via upload

---
+++ class149.py
@@ -0,0 +1,21 @@
+# functions with all parameters
+# very important to understand
+
+# PADK
+
+# parameters
+# *args
+# default parameters
+# **kwargs
+
+def func(name, *args, last_name = 'unknown', **kwargs):
+    print(name)
+    print(args)
+    print(last_name)
+    print(kwargs)
+
+func('harshit', 1,2,3, a=1, b=2)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
